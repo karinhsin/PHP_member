ed36e7422aa1a33727c6516824cf9c816c158d93
Karin Hsin <karinhsin@gmail.com>
2021-09-05 16:28:52 +0800

    新增會員中心首頁

---
+++ index_.php
@@ -0,0 +1,11 @@
+<?php
+include __DIR__ . '/partials/init.php';
+$title = '首頁';
+?>
+<?php include __DIR__ . '/partials/html-head.php';?>
+<?php include __DIR__ . '/partials/navbar.php';?>
+<div class="container">
+<h2>HELLO</h2>
+</div>
+<?php include __DIR__ . '/partials/script.php';?>
+<?php include __DIR__ . '/partials/html-foot.php';?>
